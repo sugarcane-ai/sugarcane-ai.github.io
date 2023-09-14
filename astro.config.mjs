9111bcc133b15872635998c6651ce4922b5beb44
Ankur Agarwal <signup.ankur@gmail.com>
2023-09-14 18:14:31 +0530

    upgraded to astro 3.xx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import image from "@astrojs/image";
+// import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -22,9 +22,9 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
+    // image({
+    //   serviceEntryPoint: "@astrojs/image/sharp",
+    // }),
     AutoImport({
       imports: [
         "@/shortcodes/Button",
