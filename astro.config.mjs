9ae764a82518e833597dfb2bd31eb8a17e6b7583
Ankur Agarwal <signup.ankur@gmail.com>
2023-09-17 15:04:30 +0530

    Added docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,28 +8,81 @@ import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
+import starlight from "@astrojs/starlight";
 
-// import cloudflare from "@astrojs/cloudflare";
+const site = 'https://astro.build/'
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
+  site: config.site.base_url ? config.site.base_url : "http://sugarcaneai.dev",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   integrations: [
-    react(), 
+    
+    react(),
+    // 
     sitemap({
       filter: (page) => !page.startsWith('https://sugarcaneai.dev/x')
       // filter: (page) => page !== 'https://stargazers.club/secret-vip-lounge/',
-    }), 
+    }),
+    
     tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }),
-  AutoImport({
-    imports: ["@/shortcodes/Button", "@/shortcodes/Accordion", "@/shortcodes/Notice", "@/shortcodes/Video", "@/shortcodes/Youtube", "@/shortcodes/Tabs", "@/shortcodes/Tab"]
-  }), mdx()],
+      config: {
+        applyBaseStyles: false
+      }
+    }), 
+    AutoImport({
+      imports: [
+        "@/shortcodes/Button", 
+        "@/shortcodes/Accordion", 
+        "@/shortcodes/Notice", 
+        "@/shortcodes/Video", 
+        "@/shortcodes/Youtube", 
+        "@/shortcodes/Tabs", 
+        "@/shortcodes/Tab"
+      ]
+    }), 
+    // mdx(),
+    starlight({
+      title: 'Sugarcane AI Docs',
+      favicon: '/public/images/sugar/favicon.png',
+      editLink: {
+				baseUrl: 'https://github.com/sugarcane-ai/sugarcane-ai.github.io/edit/main/src/content/docs/',
+			},
+      logo: {
+				light: '/public/images/sugar/logos/full-small.png',
+				dark: '/public/images/sugar/logos/full-small.png',
+				replacesTitle: true,
+			},
+			social: {
+				github: 'https://github.com/sugarcane-ai',
+				discord: 'https://discord.gg/TZw4GDrG',
+			},
+			// head: [
+      //   {
+			// 		tag: 'title',
+			// 		content: 'Sugarcane AI Documentation'
+			// 	},
+			// 	{
+			// 		tag: 'meta',
+			// 		attrs: { property: 'og:image', content: '/public/images/sugar/logos/full-small.png' },
+			// 	},
+			// 	{
+			// 		tag: 'meta',
+			// 		attrs: { property: 'twitter:image', content: site + '/public/images/sugar/logos/full-small.png' },
+			// 	},
+			// ],
+      sidebar: [
+		    // An external link to the Astro website.
+        {
+          label: 'Arcitecture',
+          items: [
+            { label: 'Components', link: '/guides/components/' },
+          ],
+        },
+      ]
+    }),
+  ],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, {
       test: "Table of contents"
@@ -39,7 +92,5 @@ export default defineConfig({
       wrap: true
     },
     extendDefaultPlugins: true
-  },
-  // output: "server",
-  // adapter: cloudflare()
+  }
 });
