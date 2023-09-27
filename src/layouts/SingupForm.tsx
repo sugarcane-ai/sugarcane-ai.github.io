d04635f45e39d07a533ccdd60dba636998c4d112
Saurav Jha <89625052+jsaurav7@users.noreply.github.com>
2023-09-28 00:02:05 +0530

    Develop (#11)
    * update join waitlist ui
    
    * update
    
    * Fixed package.json
    
    * Add files via upload
    
    * Add files via upload
    
    * Delete public/images/sugar/mono-vs-micro.png
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update readme.md
    
    added images/ index section/ The links are not working. need to be updated with final links
    
    * Update readme.md
    
    * Update readme.md
    
    * Add files via upload
    
    * Update readme.md
    
    * Update LICENSE
    
    * Update readme.md
    
    * Update readme.md
    
    * Update readme.md
    
    ---------
    
    Co-authored-by: Ankur Agarwal <signup.ankur@gmail.com>
    Co-authored-by: ravinderk1191 <145797535+ravinderk1191@users.noreply.github.com>

--- src/layouts/SingupForm.tsx
+++ src/layouts/SingupForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import '../styles/Modal.css'
 
 const host = import.meta.env.PUBLIC_SIGNUP_FORM_HOST
 
@@ -45,49 +46,108 @@ const SignupForm = ({ isOpen, onRequestClose }) => {
     };
 
     return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={onRequestClose}
-            contentLabel="Join Waitlist"
-            style={customStyles}
-        >
-                        
-            <div >
-                <button
-                    onClick={onRequestClose} // Close the modal when this button is clicked
-                    style={{ float: 'right' }}
-                    >
-                    Close
-                </button>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email" className="form-label">
-                        Join the Wait List
-                    </label>
-                    
-                    <input
-                        type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                    
-                    <button type="submit" className="btn btn-primary">Join</button>
-                </form>
-            </div>
-            {success && (
-                <p>Added to wait list successfully !</p>
-            )}
+        <div>
+            {isOpen && <div className='modal-window'>
+                <>
+                    {success && <>
+                        <button className='modal-close' onClick={onRequestClose}>X</button>
+                        <div className="mx-auto text-center">
+                            <img
+                                width="80px"
+                                height="80px"
+                                className="mx-auto mb-3"
+                                src="/images/sugar/sugarcane-news.png"
+                                alt="no-search-found"
+                            />
+                            <h2 className="mt-12" style={{ color: 'black', align: 'end', 'text-align': 'center', 'font-size': '2em' }} >Joined Successfully, Thanks !</h2>
+                        </div>
+                    </>}
+                    {!success && <>
+                        <button className='modal-close' onClick={onRequestClose}>X</button>
+                        <div className="mx-auto text-center">
+                            <img
+                                width="80px"
+                                height="80px"
+                                className="mx-auto mb-3"
+                                src="/images/sugar/sugarcane-news.png"
+                                alt="no-search-found"
+                            />
+                            <h2 className="mb-2" style={{ color: 'black', align: 'end', 'text-align': 'center', 'font-size': '1.5em' }} >Get exclusive early access !</h2>
+                            <h2 className="mb-6" style={{ color: 'black', align: 'end', 'text-align': 'center', 'font-size': '1.3em' }}>Join the SugarcaneAI waitlist now !</h2>
 
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    type="email"
+                                    style={{
+                                        "font-family": "inherit",
+                                        "font-size": "1rem",
+                                        "font-weight": "400",
+                                        "line-height": "inherit",
+                                        "width": "100%",
+                                        "height": "auto",
+                                        "padding": "0.75rem 1.25rem",
+                                        "border-radius": "2rem",
+                                        "color": "black",
+                                        "background": "white",
+                                        "text-transform": "unset",
+                                        "text-rendering": "optimizeLegibility"
+                                    }}
+                                    id="email"
+                                    placeholder='Email'
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
 
-        </Modal>
-    );
+                                <div>
+                                    <button className='submitButton mt-4' type="submit">Submit Now</button>
+                                </div>
+                            </form>
+                        </div>
+                    </>
+                    }
+                </>
+            </div>}
+        </div >
+    )
+
+    // return (
+    //     <div>
+    //         {isOpen && (<div className="modal" >
+    //             <button
+    //                 onClick={onRequestClose} // Close the modal when this button is clicked
+    //                 style={{ float: 'right' }}
+    //             >
+    //                 Close
+    //             </button>
+    //             <form onSubmit={handleSubmit}>
+    //                 <label htmlFor="email" className="form-label">
+    //                     Join the Wait List
+    //                 </label>
+
+    //                 <input
+    //                     type="email"
+    //                     id="email"
+    //                     value={email}
+    //                     onChange={(e) => setEmail(e.target.value)}
+    //                     required
+    //                 />
+
+    //                 <button type="submit" className="btn btn-primary">Join</button>
+    //             </form>
+    //         </div>)
+    //         }
+    //         {success && (
+    //             <p>Added to wait list successfully !</p>
+    //         )}
+    //         </div>
+    // );
 };
 
-function JoinWaitlist({ klass = 'btn btn-primary mt-5'}) {
+function JoinWaitlist({ klass = 'btn btn-primary mt-5' }) {
     const [isOpen, setIsOpen] = useState(false);
 
-    const handleOpenModal = async(e) => {
+    const handleOpenModal = async (e) => {
         e.preventDefault();
         setIsOpen(true);
     };
@@ -98,12 +158,12 @@ function JoinWaitlist({ klass = 'btn btn-primary mt-5'}) {
     };
 
     return (
-        <>  
+        <>
             <a
-              className={klass}
-              href={"#"}
-              onClick={handleOpenModal}
-            style={{ marginLeft: "1rem" }}
+                className={klass}
+                href={"#"}
+                onClick={handleOpenModal}
+                style={{ marginLeft: "1rem" }}
             >Join Waitlist</a>
             {/* <button
                 onClick={handleOpenModal}
